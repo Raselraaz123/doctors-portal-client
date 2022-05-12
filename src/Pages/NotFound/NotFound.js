6a539af284013a5b7714c9a7bea4561c4aaf3cf2
Raselraaz123 <raselraaz91@gmail.com>
2022-05-12 16:36:07 +0600

    firebase install and .env.local file add

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import page404 from '../../assets/images/page404.png'
 const NotFound = () => {
   return (
     <div>
-      <h1>Page Not Found</h1>
+      <h1 className='text-4xl font-bold font-serif text-red-500 text-center mt-28 mb-5' >Page Not Found</h1>
+      <div>
+        <img className='flex justify-center mx-auto' src={page404} alt="" />
+      </div>
     </div>
   );
 };
